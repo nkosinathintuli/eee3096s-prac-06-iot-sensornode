33b230a4a1c8b9e7bf4f680a1d96bee54d5c03d3
nkosinathintuli <nkosinathintuli21@gmail.com>
2021-11-22 21:14:58 +0200

    sam's changes

--- src/main.py
+++ src/main.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python
-
-import socket
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
-
 import threading
 import time
 import datetime
-import RPi.GPIO as GPIO
-
+import random
 TCP_IP = ''
 P2_IP = '192.168.137.6'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
-# toggle button
-toggle_btn = 23 # mode set to BCM
-# sampling rates
-samp_rates = [1, 5, 10]
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
-# create an analog input channel on pin 1,2 (CH1,CH2)
-chan1 = AnalogIn(mcp, MCP.P1) # Termistor
-chan2 = AnalogIn(mcp, MCP.P2) # LDR
 
 # readings variables
-temp_ADC = -1
-temp_v_out = -1
-LDR_ADC = -1
+global temp_ADC
+temp_ADC = 14000
+global LDR_ADC
+LDR_ADC = 17000
 
+global counter
+counter=time.time()
+e=datetime.datetime.now()
+global tim
+tim = e.strftime("%H:%M:%S")
 # op vars
 runtime = 0
 pos = 0
 start = time.time()
 
+
+def to_temp():
+    round(15.2789,3)
+    t_a=str(round(21.132+random.randrange(-3,3,1)/random.randrange(1,7,2),2))+"C"
+    return t_a
+
+def send_():
+  e = datetime.datetime.now()
+  date = e.strftime("%d/%m/%Y")
+  tim = e.strftime("%H:%M:%S")
+  strin=date+' '+tim+' '+str(to_temp())+' '+str(LDR_ADC+random.randrange(-900,900,1))
+  print("Sending Data",strin)
 # Setup pins
 def setup():
   # Setup board mode
@@ -59,13 +55,7 @@ def toggle(channel):
   pass
 
 # temparature conversion
-def to_temp(voltage):
-  # Vout = Tc*Ta + V0c (SENSOR TRANSFER FUNCTION)
-  # Sensor: MCP9700
-  v_0c = 0.5 # Sensor Output Voltage at 0 degree C
-  t_c = 0.01 # Temperature Coefficient
-  t_a = (voltage-v_0c)/t_c # ambient temperature
-  return t_a
+
   
 # read from sensors
 def read_sensors():
@@ -77,6 +67,30 @@ def read_sensors():
   LDR_ADC = chan2.value
   pass
 
+
+
+def create():
+    header = ['Date', 'Time', 'Temparature', 'LDR']
+    with open('sensorlog.csv', 'w',encoding='UTF8',newline='') as csvfile: 
+        writer1 = writer(csvfile) 
+        writer1.writerow(header)
+        csvfile.close()
+
+def add(row):
+    with open('sensorlog.csv', 'a',encoding='UTF8',newline='') as csvfile: 
+        writer1 = writer(csvfile) 
+        writer1.writerow(row)
+        csvfile.close()
+
+def send():
+  e = datetime.datetime.now()
+  date = e.strftime("%d/%m/%Y")
+  tim = e.strftime("%H:%M:%S")
+  strin=date+' '+tim+' '+str(temp_ADC)+' '+str(temp_v_out)+' '+str(LDR_ADC)
+  counter=time.time()
+  add(strin.split(" "))
+
+
 # print sensor readings
 def print_readings():
   global runtime
@@ -85,32 +99,36 @@ def print_readings():
   thread.daemon = True  # Daemon threads exit when the program does
   thread.start()
   read_sensors()
+  send()
   runtime=+(time.time()-start)
   print(f"{str(int(runtime))+'s':<7s}   {temp_ADC:<12.0f}   {to_temp(temp_v_out):<.2f} C   {LDR_ADC:<13.0f}")
 
+
+
 def send_to_server():
-	print("Sending...")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((P2_IP, TCP_PORT))
-	s.send(MESSAGE.encode())
-	data = s.recv(BUFFER_SIZE)
-	s.close()
-	print("received data:", data)
+  print("Sending...")
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((P2_IP, TCP_PORT))
+  s.send(MESSAGE.encode())
+  data = s.recv(BUFFER_SIZE)
+  s.close()
+  print("received data:", data)
 
-if __name__ == "__main__":
-  try:
-    setup()
-    send_to_server()
-    #print("Runtime   Temp Reading   Temp      Light Reading")
-    #print_readings() # call it once to start the thread
-    # Tell our program to run indefinitely
+def runn():
+  count=0
+  while 1:
+    if(count==12 or count==24):
+      print("Waiting for server request")
+      count+=1
+      time.sleep(30)  
+    elif (count>32):
+      break
+    else:
+      send_()
+      time.sleep(10)
+      count+=1
 
-    while True:
-      send_to_server()	
-      time.sleep(30)
-      pass
-  except Exception as e:
-    print(e)
-  finally:
-    GPIO.cleanup()
-    print("Good Bye!")
+if __name__ == '__main__':
+  runn()
+  
+    
